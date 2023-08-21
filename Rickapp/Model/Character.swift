b51faa573db7d26664601c58bcacdbd7e84e6dc2
Wojciech Mokwiński <wojciech.mokwinski@goodylabs.com>
2023-08-21 10:08:04 +0200

    Small changes

--- Rickapp/Model/Character.swift
+++ Rickapp/Model/Character.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-struct Charsacter {
+struct Character: Decodable {
+    let name: String
+    let status: String
+    let species: String
+    let location: Location
+    let origin: Origin
+    let image: String
     
+    enum CodingKeys: String, CodingKey {
+        case name, status, species, location, origin, image = "image"
+    }
+}
+
+struct Location: Decodable {
+    let name: String
+}
+
+struct Origin: Decodable {
+    let name: String
+}
+
+struct CharacterResponse: Decodable {
+    let results: [Character]
 }
