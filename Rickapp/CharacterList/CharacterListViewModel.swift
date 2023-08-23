9f5f627a6c9238d6c7a71893eb07d4b986b3c13f
wojciech.mokwinski <wojciech.mokwinski@goodylabs.com>
2023-08-23 18:19:49 +0200

    Applied combine

--- Rickapp/CharacterList/CharacterListViewModel.swift
+++ Rickapp/CharacterList/CharacterListViewModel.swift
@@ -6,9 +6,18 @@
 //
 
 import Foundation
-import UIKit
 import Combine
 
 class CharacterListViewModel: BaseViewModel {
+    @Published var characters: [Character] = []
+    var cancellables: Set<AnyCancellable> = []
     
+    override func bindToData() {
+        CharacterService.shared.getCharacter()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }) { [weak self] characters in
+                self?.characters = characters
+            }
+            .store(in: &cancellables)
+    }
 }
