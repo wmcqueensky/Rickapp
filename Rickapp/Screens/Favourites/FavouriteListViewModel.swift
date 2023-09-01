2e93ba47776972b8ea52a74cbcb3906eef277606
wojciech.mokwinski <wojciech.mokwinski@goodylabs.com>
2023-09-01 02:26:57 +0200

    Change

--- Rickapp/Screens/Favourites/FavouriteListViewModel.swift
+++ Rickapp/Screens/Favourites/FavouriteListViewModel.swift
@@ -9,34 +9,5 @@ import Foundation
 import Combine
 
 class FavouriteListViewModel: BaseViewModel {
-    private var isLoadingNextPage = false
-    private var nextPage: String?
-    private var previousPage: String?
-    var charactersPublisher = CurrentValueSubject<[Character], Never>([])
-    var isFollowed = PassthroughSubject<Bool, Never>()
-
-    override func bindToData() {
-        super.bindToData()
-        
-    }
     
-    func getNextCharactersPage() {
-        guard !isLoadingNextPage, let nextPage = nextPage else { return }
-        if nextPage.isEmpty { return }
-        isLoadingNextPage = true
-        CardService.shared.getNextCharactersPage(url: nextPage)
-            .sink(receiveCompletion: { _ in }) { [weak self] characterList in
-                guard let self = self else { return }
-                let existingCharacters = self.charactersPublisher.value
-                let newCharacters = characterList.results ?? []
-                self.charactersPublisher.send(existingCharacters + newCharacters)
-                self.nextPage = characterList.info?.next ?? ""
-                self.isLoadingNextPage = false
-            }
-            .store(in: &cancellables)
-    }
-    
-    func getCharacterById(_ characterId: Int) {
-        AppNavigator.shared.navigate(to: MainRoutes.card(characterId: characterId), with: .push, animated: true)
-    }
 }
