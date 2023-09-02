4f54b7648480476ff3a74f9c52fa9c601c4514af
wojciech.mokwinski <wojciech.mokwinski@goodylabs.com>
2023-09-02 19:52:13 +0200

    UI changes

--- Rickapp/BaseElements/BaseView/BaseViewModel.swift
+++ Rickapp/BaseElements/BaseView/BaseViewModel.swift
@@ -15,5 +15,9 @@ class BaseViewModel: NSObject {
         AppNavigator.shared.navigate(to: MainRoutes.details(characterId: characterId), with: .push, animated: true)
     }
     
+    func locationButtonTapped(_ locationUrl: String) {
+        AppNavigator.shared.navigate(to: MainRoutes.location(url: locationUrl), with: .push, animated: true)
+    }
+    
     func bindToData() {}
 }
