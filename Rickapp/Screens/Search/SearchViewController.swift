91d6cc2a4dcdfa0ca5645c32f2f0d958e3061563
wojciech.mokwinski <wojciech.mokwinski@goodylabs.com>
2023-09-22 18:44:45 +0200

    Customisation of MenuViewController

---
+++ Rickapp/Screens/Search/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  Rickapp
+//
+//  Created by Wojciech Mokwiński on 22/09/2023.
+//
+
+import UIKit
+import Moya
+import SnapKit
+
+class SearchViewController: BaseViewController<SearchViewModel> {
+    
+    override func bindToViewModel() {
+        super.bindToViewModel()
+    }
+    
+    override func setupViews() {
+        super.setupViews()
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+    }
+}
