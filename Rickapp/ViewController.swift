b065af22693f318f6ac2a5a380c23a8ce1d3daea
Wojciech Mokwiński <mokwinskiwojciech0@gmail.com>
2024-04-07 22:32:04 +0200

    No Storyboard

--- Rickapp/ViewController.swift
+++ Rickapp/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy private var sampleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello World!"
+        label.textColor = UIColor.white
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.black
+        self.view.addSubview(self.sampleLabel)
+        self.setUpConstraints()
     }
 
+    func setUpConstraints() {
+        let sampleLabelConstraints = [
+            self.sampleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.sampleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(sampleLabelConstraints)
+    }
 
 }
 
